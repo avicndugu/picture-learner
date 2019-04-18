3cebb63bbb4f3c8c970355688bc15cb6b1c68734
avicndugu <avicnduguh@gmail.com>
2019-04-18 15:11:04 +0300

    Resolve most of the conflicts. No data collection added

--- views-contr.js
+++ views-contr.js
@@ -1,4 +1,9 @@
 // First view has logo, tag line and a button to start.
+// Next tasks will be on the logic code and some of the view code
+// 1. Find a way of collecting the text from the input value.
+// 2. Clear the value when the next button is pressed.
+// 3. Turn the data into JSON string when the submit button is pressed
+// 4.
 const header=document.getElementById('header');
 const subject= document.getElementById('subject');
 const firstSubject=document.getElementById('first-subject');
@@ -13,7 +18,6 @@ const english=document.getElementById("english");
 const swahili=document.getElementById("swahili");
 const nav=document.getElementById("navigation");
 // document.getElementById('logo').addEventListener('load', firstView);
-exit.addEventListener('click', firstView);
 console.log(firstSubject.textContent);
 let state;
 const errorLang=document.getElementById("error-language");
@@ -24,7 +28,7 @@ const errorLang=document.getElementById("error-language");
 
 firstView();
 function firstView(){
-    header.style.display="inline-block";
+    header.style.display="block";
     exit.style.display="none";
     subject.style.display="none";
     firstSubject.textContent="NEXT";
@@ -40,15 +44,16 @@ function secondView(){
     //     errorLang.textContent="Choose a language!";
     //     console.log(state);
     // } else {
+        header.style.display="none";
         languages.style.display="none";
         // other.style.display="none";
-        action.style.display="none";
-        tagline.style.display="none";
+        // action.style.display="none";
+        // tagline.style.display="none";
         nav.style.display="block";
         submit.style.display="inline-block";
-        subject.style.display="inline-block";   
-
-        console.log(state);    
+        subject.style.display="inline-block";
+        exit.style.display="inline-block";
+        console.log(state);
     // }
 
     // if (firstSubject.textContent==='START'){
@@ -66,34 +71,49 @@ function secondView(){
     // }
 }
 function helpView(){
-    subject.style.display="inline-block";   
-    header.style.display="none";
+    // subject.style.display="inline-block";   
+    // header.style.display="none";
     // contribute.style.display="none";
-    contribution.style.display="block";
+    // contribution.style.display="block";
 
 }
 // Second view has the name of the object the image below it and the next button.
-document.getElementById('first-subject').addEventListener('click', secondView);
+const next=document.getElementById('first-subject');
+next.addEventListener('click', function(){
+    secondView();
+    // 
+
+});
 
 other.addEventListener("click",function(){
         contribution.style.display="block";
         languages.style.display="none";
         nav.style.display="block";
         console.log(document.langName.language.value.length);
-
+        setLang(document.langName.language.value.length);
 });
 english.addEventListener("click",function(){
     state=1;
+    languages.style.display="none";
     nav.style.display="block";
     // secondView();
+    newDict["language"]=setLang(this.innerText);
+    // document.langName.language.value;
+    // console.log(setLang(this.innerText));
 });
 
 swahili.addEventListener("click",function(){
     state=1;
+    languages.style.display="none";
     nav.style.display="block";
 });
+
+exit.addEventListener('click', firstView);
+
+// exit.addEventListener('click', firstView);
+
 // document.getElementById('contribute').addEventListener('click', helpView);
 // the final view is the same as the previous one but has an exit button.
 // submit.addEventListener('click', function(){
-    
+
 // });
