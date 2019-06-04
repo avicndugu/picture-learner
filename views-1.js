a5e4c3f488b06302d42f239fb10c523a66e5d152
avicndugu <avicnduguh@gmail.com>
2019-06-04 22:57:13 +0300

    Going back basics CSS and html editing

--- views-1.js
+++ views-1.js
@@ -3,8 +3,8 @@ const header=document.getElementById('header');
 const subject= document.getElementById('subject');
 const firstSubject=document.getElementById('first-subject');
 const exit=document.getElementById('exit');
-// const contribute=document.getElementById('contribute');
-// const contribution= document.getElementById('contribution');
+const contribute=document.getElementById('contribute');
+const contribution= document.getElementById('contribution');
 const languages=document.getElementById("languages");
 const swahili = document.getElementById('swahili');
 const english = document.getElementById('english');
@@ -23,7 +23,7 @@ function firstView(){
 	exit.style.display="none";
 	subject.style.display="none";
 	firstSubject.textContent="NEXT";
-	// contribute.style.display="inline-block";
+	contribute.style.display="inline-block";
 	languages.style.display="block";
 	navigation.style.display="none";
 }
@@ -32,7 +32,7 @@ function secondView(){
 		firstSubject.textContent="NEXT";
 		subject.style.display="inline-block";	
 		header.style.display="none";
-		// contribute.style.display="none";
+		contribute.style.display="none";
 		languages.style.display="none";
 		exit.style.display="inline-block";
 		// console.log(22);
@@ -45,22 +45,22 @@ function secondView(){
 function helpView(){
 	subject.style.display="inline-block";	
 	header.style.display="none";
-	// contribute.style.display="none";
-	// contribution.style.display="block";
+	contribute.style.display="none";
+	contribution.style.display="block";
 
 }
 // Second view has the name of the object the image below it and the next button.
 document.getElementById('first-subject').addEventListener('click', secondView);
-// document.getElementById('contribute').addEventListener('click', helpView);
+document.getElementById('contribute').addEventListener('click', helpView);
 // the final view is the same as the previous one but has an exit button.
 english.addEventListener("click", function(){
 	// Choose language
 	navigation.style.display="block";
-	// contribute.style.display="none";
+	contribute.style.display="none";
 });
 swahili.addEventListener("click", function(){
 	// Choose language
 	navigation.style.display="block";
-	// contribute.style.display="none";	
+	contribute.style.display="none";	
 });
 
