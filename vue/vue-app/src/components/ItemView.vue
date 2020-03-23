6cb79882a0675c13f0fd5c7216d667415f39d1d5
avicndugu <avicnduguh@gmail.com>
2020-03-23 16:10:26 +0300

    Adapted page 1 and 3 app to view

---
+++ vue/vue-app/src/components/ItemView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="item-view">
+        <ol v-for="item in items" :key="item.id">
+          <li>{{ item.name }}</li>
+          <li>{{ item.url }}</li>
+          <li>{{ item.audio }}</li>
+        </ol>
+        <div id="subject" class="first-view">
+            <h2 class="title">Loading</h2>
+            <img src="img/place-holder.png">
+            <audio id="sounds"></audio>
+        </div>
+        <div class="button-group" id="navigation">
+            <button id="player" class="button"><span class="play"></span></button>
+            <br>
+            <br>
+            <button id="back" class="button">Back</button>
+            <button id="first-subject" class="button"></button>
+            <button id="exit" class="first-view button">EXIT</button>
+            <br>
+        </div>
+    </div>
+</template>
+<script>
+    export default{
+        name: "item-view",
+        props: {
+            items: Array,
+        }
+    }
+
+</script>
+
+<style>
+</style>
